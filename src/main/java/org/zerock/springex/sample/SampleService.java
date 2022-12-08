fa86dcb00c709f8eb63538cc59e91c8c62963623
Yeeun Kim <maemae2@naver.com>
2022-12-08 14:54:55 +0900

    4-1 실습 : 실습_03 - @Service, @Repository, 생성자 주입 방식

--- src/main/java/org/zerock/springex/sample/SampleService.java
+++ src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,11 +1,20 @@
 package org.zerock.springex.sample;
 
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 @ToString
+@RequiredArgsConstructor
 public class SampleService {
 
-    @Autowired
-    private SampleDAO sampleDAO;
+//    // @Autowired 를 이용한 필드 주입
+//    @Autowired
+//    private SampleDAO sampleDAO;
+
+    // 생성자 주입 방식 : Lombok의 @RequiredArgsConstructor 사용 (final + 생성자 만들기)
+    private final SampleDAO sampleDAO;
+
 }
